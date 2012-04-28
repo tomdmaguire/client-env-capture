11ef2225d608207103c8e3f7b987f6069dbf0a95
Tom Maguire <tom@musicglue.com>
2012-04-28 09:28:18 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CustomerEnv::Application.config.secret_token = '737f1c0226439c1bf48a3f9aab4baba0265068d62b5a50822e5df865eab64dae2feabd0b7611d11e72c2c52db2660c054a59299393cfc043bf9c7b5b1df325b1'
